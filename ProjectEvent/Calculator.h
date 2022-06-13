5cf860570d1f556adfc991d4fc1bd7ea822e07f8
kapikabudasasa@yahoo.com <kapikabudasasa@yahoo.com>
2022-06-12 22:11:40 -0400

    Add CalculatorApp

---
+++ ProjectEvent/Calculator.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "wx\wx.h"
+#include"cMain.h"
+class Calculator: public wxApp
+{
+public:
+	Calculator();
+	//~Calculator();
+private:
+	cMain* m_calculate = nullptr;
+
+public:
+	virtual bool OnInit();
+
+};
+
---
+++ ProjectEvent/cMain.h
@@ -0,0 +1,34 @@
+#pragma once
+#include"wx\wx.h"
+class cMain: public wxFrame
+{
+private:
+	wxButton* xbtn;
+	wxButton* xbtn1;
+	wxButton* xbtn2;
+	wxButton* xbtn3;
+	wxButton* xbtn4; 
+	wxButton* xbtn5; 
+	wxButton* xbtn6;
+	wxButton* xbtn7;
+	wxButton* xbtn8; 
+	wxButton* xbtn9;
+	wxButton* xbtn10; 
+	wxButton* xbtn11;
+	wxButton* xbtn12;
+	wxButton* xbtn13; 
+	wxButton* xbtn14;
+	wxButton* xbtn15;
+	wxButton* xbtn16;
+	wxButton* xbtn17;
+	wxButton* xbtn18; 
+	wxButton* xbtn19;
+	wxTextCtrl* wtext;
+public:
+	cMain();
+	wxDECLARE_EVENT_TABLE();
+	void OnButtonClick(wxCommandEvent& evt);
+	//~cMain();
+
+};
+
